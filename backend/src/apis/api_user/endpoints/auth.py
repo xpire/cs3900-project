750ef72fd55113b0bcfb13c08b28a58b2e69d979
george99524 <georgebai99524@gmail.com>
2020-10-15 22:45:54 +1100

    added API for testing user tokens

--- backend/src/apis/api_user/endpoints/auth.py
+++ backend/src/apis/api_user/endpoints/auth.py
@@ -1,13 +1,27 @@
 from datetime import timedelta
 from typing import Any
 
-from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi import APIRouter, Body, Depends, HTTPException, Query, Header
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+
+import firebase_admin
+from firebase_admin import auth, credentials
 
 # Import from crud, model, etc..
 
 router = APIRouter()
 
+cred = credentials.Certificate("ecksdee-firebase.json")
+firebase_admin.initialize_app(cred)
+
 # Start implementation here, this file handles authentication
 
+
+@router.get("/check")
+async def get_user(id_token: str = Header(None)):
+
+    decoded_token = auth.verify_id_token(id_token)
+    uid = decoded_token["uid"]
+
+    return uid

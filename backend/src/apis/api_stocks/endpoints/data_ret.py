04fd863829db6b2ad82d72fb2551125176fa78ac
George Bai <georgebai99524@gmail.com>
2020-10-15 14:32:41 +1100

    rename to stocks.py and delete search.py

--- backend/src/apis/api_stocks/endpoints/data_ret.py
+++
@@ -1,9 +0,0 @@
-from typing import Any, List
-
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-
-
-router = APIRouter()
-
-# Start implementation here, this file handles batch and single stock data retrieval

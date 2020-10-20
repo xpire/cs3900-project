456afc2cfc87b339e998ede90c36d909018bbe39
Song <z5165313@student.unsw.edu.au>
2020-10-21 01:15:25 +1100

    trade skeleton done

--- backend/src/api/endpoints/trade.py
+++ backend/src/api/endpoints/trade.py
@@ -1,22 +1,86 @@
-import json
-import os
 from typing import Any, List
 
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
-from src import crud
-from src.api.deps import get_db
+from src import crud, models, schemas
+from src.api.deps import check_symbol, decode_token, get_current_user_m, get_db
 from src.core.config import settings
-from src.real_time_market_data.data_provider import (
-    CompositeDataProvider,
-    LatestClosingPriceProvider,
-    RealTimeDataProvider,
-    SimulatedDataProvider,
-    SimulatedStock,
-)
-from twelvedata import TDClient
-
-API_URL = "https://api.twelvedata.com"
-API_KEY = settings.TD_API_KEY
+from src.db.session import SessionLocal
 
 router = APIRouter()
+
+
+@router.post("/market/buy")
+async def market_buy(
+    symbol: str = Depends(check_symbol),
+    quantity: int,
+    user: models.User = Depends(get_current_user_m),
+    db: Session = Depends(get_db),
+):
+
+    return {"result": "success"}
+
+
+@router.post("/market/sell")
+async def market_sell(
+    symbol: str = Depends(check_symbol),
+    quantity: int,
+    user: models.User=Depends(get_current_user_m),
+    db: Session = Depends(get_db),
+):
+
+    return {"result": "success"}
+    
+@router.post("/market/short")
+async def market_short(
+    symbol: str = Depends(check_symbol),
+    quantity: int,
+    user: models.User=Depends(get_current_user_m),
+    db: Session = Depends(get_db),
+)
+    return {"result": "success"}
+
+@router.post("/market/cover")
+async def market_cover(
+    symbol: str = Depends(check_symbol),
+    quantity: int,
+    user: models.User=Depends(get_current_user_m),
+    db: Session = Depends(get_db),
+)
+    return {"result": "success"}
+
+@router.post("/limit/buy")
+async def limit_buy(
+    symbol: str = Depends(check_symbol),
+    quantity: int,
+    user: models.User=Depends(get_current_user_m),
+    db: Session = Depends(get_db),
+)
+    return {"result": "success"}
+
+@router.post("/limit/sell")
+async def limit_sell(
+    symbol: str = Depends(check_symbol),
+    quantity: int,
+    user: models.User=Depends(get_current_user_m),
+    db: Session = Depends(get_db),
+)
+    return {"result": "success"}
+
+@router.post("/limit/short")
+async def limit_short(
+    symbol: str = Depends(check_symbol),
+    quantity: int,
+    user: models.User=Depends(get_current_user_m),
+    db: Session = Depends(get_db),
+)
+    return {"result": "success"}
+
+@router.post("/limit/cover")
+async def limit_cover(
+    symbol: str = Depends(check_symbol),
+    quantity: int,
+    user: models.User=Depends(get_current_user_m),
+    db: Session = Depends(get_db),
+)
+    return {"result": "success"}

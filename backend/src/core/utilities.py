af8781329c96437cf93d87f8629a305dd6b6b96b
Even Tang <even.tang@ofx.com>
2020-10-20 15:57:03 +1100

    watchlist database feature complete

---
+++ backend/src/core/utilities.py
@@ -0,0 +1,57 @@
+"""
+    File name: utilities.py
+    Author: Peiyu Tang
+    Date created: 10/18/2020
+    Python Version: 3.7.3
+    Purpose: Some utility functions used by core 
+        features, maybe it can be used by other modules too 
+"""
+from collections import defaultdict
+import inspect
+import os
+import logging
+
+
+def find_path_curr_f() -> str:
+    """
+    Find the absolute path of currently executed file
+    """
+    filename = inspect.getframeinfo(inspect.currentframe()).filename
+    abs_path = os.path.dirname(os.path.abspath(filename))
+    return filename, abs_path
+
+
+def log_msg(msg: str, level: str) -> None:
+    """
+    Log a message to console to indicate status for production, level can be used
+    are {DEBUG < INFO < WARNING < ERROR < CRITICAL}. Please note that this only
+    handles printing not actually raising any errors
+    """
+    logger = logging.getLogger("backend logger")
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    ch.setLevel(logging.DEBUG)
+
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+
+    plvs = defaultdict(lambda: None)
+    log_lvs = {
+        "DEBUG": logger.debug,
+        "INFO": logger.info,
+        "WARNING": logger.warning,
+        "ERROR": logger.error,
+        "CRITICAL": logger.critical,
+    }
+
+    for key, item in log_lvs.items():
+        plvs[key] = item
+
+    if plvs[level] == None:
+        logger.critical("No specified logger level.")
+    else:
+        plvs[level](msg)
--- backend/src/crud/crud_user.py
+++ backend/src/crud/crud_user.py
@@ -9,12 +9,15 @@
 from typing import Any, Dict, Optional, Union
 
 # import src.models as md
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 from src.core.config import settings
+
 from src.crud.base import CRUDBase
-from src.db.session import SessionLocal
 from src.models.user import User
 from src.schemas.user import UserCreate, UserUpdate
+from src.models.watch_list import WatchList
+from src.core.utilities import log_msg
 
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
@@ -22,13 +25,13 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     Module for user/auth related CRUD operations
     """
 
-    "something"
-
-    def get_user_by_token(self, db: Session, *, uid: str) -> Optional[User]:
+    def get_user_by_uid(self, db: Session, *, uid: str) -> Optional[User]:
         """
         Return the corresponding user by token.
         """
-        return db.query(self.model).filter(self.model.uid == uid).first()  # Field is unique
+        return (
+            db.query(self.model).filter(self.model.uid == uid).first()
+        )  # Field is unique
 
     def update_balance(self, db: Session, *, db_obj: User, obj_in: UserUpdate) -> User:
         """
@@ -38,6 +41,42 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
 
     def authenticate(self, db: Session, *, email: str, password: str) -> Optional[User]:
         pass
+
+    def add_to_watch_list(self, db: Session, user_in: User, w_symbol: str) -> User:
+        """
+        Add a watchlist to the user's watchlist.
+        """
+        # BUG: doesnt handle case where w_symbol not a stock symbol, handle later maybe
+        a_wl = WatchList(user_id=user_in.uid, symbol=w_symbol)
+
+        user_in.watchlist.append(a_wl)
+        db.commit()
+        db.refresh(user_in)
+        return user_in
+
+    def delete_from_watch_list(
+        self, db: Session, *, user_in: User, w_symbol: str
+    ) -> User:
+        """
+        Delete a watchlist for this user.
+        """
+        tbr = None
+        for s in user_in.watchlist:
+            if s.symbol == w_symbol:
+                tbr = s
+                break
+
+        if tbr == None:
+            log_msg(
+                f"Deleting a non-existent stock from watchlist of User(uid = {user_in.uid})",
+                "WARNING",
+            )
+
+        else:
+            user_in.watchlist.remove(tbr)
+            db.commit()
+            db.refresh(user_in)
+        return user_in
 
 
 user = CRUDUser(User)

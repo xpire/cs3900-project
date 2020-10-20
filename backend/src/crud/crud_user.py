44687968f7d0b83ff49cf79b1426a4a5eb6a51a3
Even Tang <even.tang@ofx.com>
2020-10-20 16:18:24 +1100

    watchlist database feature minor addition.

--- backend/src/crud/crud_user.py
+++ backend/src/crud/crud_user.py
@@ -11,9 +11,11 @@ from typing import Any, Dict, Optional, Union
 # import src.models as md
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
+from sqlalchemy.util.langhelpers import symbol
 from src.core.config import settings
 
 from src.crud.base import CRUDBase
+from src.crud.crud_stock import stock
 from src.models.user import User
 from src.schemas.user import UserCreate, UserUpdate
 from src.models.watch_list import WatchList
@@ -42,17 +44,31 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     def authenticate(self, db: Session, *, email: str, password: str) -> Optional[User]:
         pass
 
+    def symbol_exist(self, db: Session, c_symbol: str):
+        """
+        Return True if the symbol exists
+        """
+        check = stock.get_stock_by_symbol(db=db, stock_symbol=c_symbol)
+
+        return True if (check != None) else False
+
     def add_to_watch_list(self, db: Session, user_in: User, w_symbol: str) -> User:
         """
         Add a watchlist to the user's watchlist.
         """
-        # BUG: doesnt handle case where w_symbol not a stock symbol, handle later maybe
-        a_wl = WatchList(user_id=user_in.uid, symbol=w_symbol)
+        if self.symbol_exist(db=db, c_symbol=w_symbol):
+            a_wl = WatchList(user_id=user_in.uid, symbol=w_symbol)
 
-        user_in.watchlist.append(a_wl)
-        db.commit()
-        db.refresh(user_in)
-        return user_in
+            user_in.watchlist.append(a_wl)
+            db.commit()
+            db.refresh(user_in)
+            return user_in
+        else:
+            log_msg(
+                f"Adding a non-existent symbol to watchlist of User(uid = {user_in.uid}).",
+                "WARNING",
+            )
+            return user_in
 
     def delete_from_watch_list(
         self, db: Session, *, user_in: User, w_symbol: str
@@ -60,23 +76,30 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         """
         Delete a watchlist for this user.
         """
-        tbr = None
-        for s in user_in.watchlist:
-            if s.symbol == w_symbol:
-                tbr = s
-                break
+        if self.symbol_exist(db=db, c_symbol=w_symbol):
+            tbr = None
+            for s in user_in.watchlist:
+                if s.symbol == w_symbol:
+                    tbr = s
+                    break
 
-        if tbr == None:
+            if tbr == None:
+                log_msg(
+                    f"Deleting a non-existent stock from watchlist of User(uid = {user_in.uid})",
+                    "WARNING",
+                )
+
+            else:
+                user_in.watchlist.remove(tbr)
+                db.commit()
+                db.refresh(user_in)
+            return user_in
+        else:
             log_msg(
-                f"Deleting a non-existent stock from watchlist of User(uid = {user_in.uid})",
+                f"Deleting a non-existent symbol from watchlist of User(uid = {user_in.uid}).",
                 "WARNING",
             )
-
-        else:
-            user_in.watchlist.remove(tbr)
-            db.commit()
-            db.refresh(user_in)
-        return user_in
+            return user_in
 
 
 user = CRUDUser(User)

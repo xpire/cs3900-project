fc216d9b620df9bbecedabb9d108d6be6de3c7c4
Ian Park <ian.isu.park@gmail.com>
2020-10-22 22:10:16 +1100

    modified start script to work on mac too

--- backend/start.sh
+++ backend/start.sh
@@ -109,7 +109,7 @@ elif [ $# -eq 1 ]; then
             title-bar; 
             read -p "Are you sure you want to nuke it ? you will lose all the data in the database...   "  res
             set-python-path
-            if [ $res == "yes" ]; then 
+            if [ $res=="yes" ]; then 
                 echo "Nuking database from the orbit..."
                 rm ../database/testdb.sqlite3 # change this later
                 echo "Recreating it now..."

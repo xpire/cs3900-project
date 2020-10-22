6f8c51da7da92f448d53671af1d2f159c3eba072
Even Tang <InvalidHero@github.com>
2020-10-22 15:30:56 +1100

    Add feature for short selling

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,5 +1,6 @@
 from .achievement import UnlockedAchievement
 from .portfolio import Portfolio
+from .short_sell import ShortSelll
 from .stock import Stock
 from .time_series import TimeSeries
 from .user import User
--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -11,7 +11,8 @@ class User(BaseModel):
     level = Column(Integer, nullable=False)
     exp = Column(Float, nullable=False)
     watchlist = relationship("WatchList", backref="user", cascade="save-update, merge, delete, delete-orphan")
-    portfolios = relationship("Portfolio", backref="user", cascade="save-update, merge, delete, delete-orphan")
+    long_positions = relationship("Portfolio", backref="user", cascade="save-update, merge, delete, delete-orphan")
+    short_positions = relationship("ShortSell", backref="user", cascade="save-update, merge, delete, delete-orphan")
     unlocked_achievements = relationship(
         "UnlockedAchievement", backref="user", cascade="save-update, merge, delete, delete-orphan"
     )

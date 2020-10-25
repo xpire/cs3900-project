969a70f261ed2f44f119daea9830f986414bee19
Song <z5165313@student.unsw.edu.au>
2020-10-25 13:14:35 +1100

    response schema added, limit structure added

--- backend/src/api/endpoints/trade.py
+++ backend/src/api/endpoints/trade.py
@@ -8,8 +8,13 @@ from src.core import trade
 from src.api.deps import check_symbol, get_current_user_dm, get_db
 from src.core.config import settings
 from src.db.session import SessionLocal
+from src.schemas.response import Response
+
+import datetime
 
 router = APIRouter()
+
+# TODO: enforce trading hours
 
 
 @router.post("/market/buy")
@@ -18,15 +23,15 @@ async def market_buy(
     symbol: str = Depends(check_symbol),
     user: domain_models.UserDM = Depends(get_current_user_dm),
     db: Session = Depends(get_db),
-):
+) -> Response:
     if quantity < 0:
-        return {"result": "cannot buy negative quantity"}
+        raise HTTPException(status_code=400, detail="Cannot buy negative quantity")
 
     curr_stock_price = trade.get_stock_price(db, symbol)
     trade_price = trade.apply_commission(curr_stock_price * quantity)
 
     if user.model.balance < trade_price:
-        return {"result": "insufficient balance"}
+        raise HTTPException(status_code=400, detail="Insufficient balance")
 
     crud_user.user.add_transaction(
         db, user.model, "long", symbol, quantity, curr_stock_price
@@ -35,7 +40,7 @@ async def market_buy(
     new_balance = user.model.balance - trade_price
     crud_user.user.update_balance(db, user.model, new_balance)
 
-    return {"result": "success"}
+    return Response("Trade successful")
 
 
 @router.post("/market/sell")
@@ -44,15 +49,13 @@ async def market_sell(
     symbol: str = Depends(check_symbol),
     user: domain_models.UserDM = Depends(get_current_user_dm),
     db: Session = Depends(get_db),
-):
+) -> Response:
 
     if quantity < 0:
-        return {"result": "cannot sell negative quantity"}
-
-    print(symbol, quantity)
+        raise HTTPException(status_code=400, detail="Cannot sell negative quantity")
 
     if not trade.check_owned_longs(user, quantity, symbol):
-        return {"result": "cannot sell more than owned"}
+        raise HTTPException(status_code=400, detail="Cannot sell more than owned")
 
     curr_stock_price = trade.get_stock_price(db, symbol)
     trade_price = trade.apply_commission(curr_stock_price * quantity, False)
@@ -62,7 +65,7 @@ async def market_sell(
     new_balance = user.model.balance + trade_price
     crud_user.user.update_balance(db, user.model, new_balance)
 
-    return {"result": "success"}
+    return Response("Trade successful")
 
 
 @router.post("/market/short")
@@ -71,15 +74,17 @@ async def market_short(
     symbol: str = Depends(check_symbol),
     user: domain_models.UserDM = Depends(get_current_user_dm),
     db: Session = Depends(get_db),
-):
+) -> Response:
     if quantity < 0:
-        return {"result": "cannot short sell negative quantity"}
+        raise HTTPException(
+            status_code=400, detail="Cannot short sell negative quantity"
+        )
 
     curr_stock_price = trade.get_stock_price(db, symbol)
     trade_price = curr_stock_price * quantity
 
     if not trade.check_short_balance(user, trade_price):
-        return {"result": "insufficient short balance"}
+        raise HTTPException(status_code=400, detail="Insufficient short balance")
 
     final_trade_price = trade.apply_commission(trade_price, False)
 
@@ -90,7 +95,7 @@ async def market_short(
     new_balance = user.model.balance + final_trade_price
     crud_user.user.update_balance(db, user.model, new_balance)
 
-    return {"result": "success"}
+    return Response("Trade successful")
 
 
 @router.post("/market/cover")
@@ -99,62 +104,80 @@ async def market_cover(
     symbol: str = Depends(check_symbol),
     user: domain_models.UserDM = Depends(get_current_user_dm),
     db: Session = Depends(get_db),
-):
+) -> Reponse:
     if quantity < 0:
-        return {"result": "cannot cover negative quantity"}
+        raise HTTPException(status_code=400, detail="Cannot cover negative quantity")
 
     curr_stock_price = trade.get_stock_price(db, symbol)
     trade_price = trade.apply_commission(curr_stock_price * quantity)
 
     if not trade.check_owned_shorts(user, quantity, symbol):
-        return {"results": "cannot cover more than owed"}
+        raise HTTPException(status_code=400, detail="Cannot cover more than owed")
 
     if user.model.balance < trade_price:
-        return {"result": "insufficient balance"}
+        raise HTTPException(status_code=400, detail="Insufficient balance")
 
     crud_user.user.deduct_transaction(db, user.model, "short", symbol, quantity)
 
     new_balance = user.model.balance - trade_price
     crud_user.user.update_balance(db, user.model, new_balance)
 
-    return {"result": "success"}
+    return Response("Trade successful")
 
 
 @router.post("/limit/buy")
 async def limit_buy(
     quantity: int,
+    limit: float,
+    expiry_date: str,
+    expiry_time: str,
     symbol: str = Depends(check_symbol),
     user: domain_models.UserDM = Depends(get_current_user_dm),
     db: Session = Depends(get_db),
-):
+) -> Response:
+    if quantity < 0:
+        raise HTTPException(status_code=400, detail="Cannot buy negative quantity")
+
+    if limit < 0:
+        raise HTTPException(status_code=400, detail="Limit value cannot be negative")
+
     return {"result": "success"}
 
 
 @router.post("/limit/sell")
 async def limit_sell(
     quantity: int,
+    limit: float,
+    expiry_date: str,
+    expiry_time: str,
     symbol: str = Depends(check_symbol),
     user: domain_models.UserDM = Depends(get_current_user_dm),
     db: Session = Depends(get_db),
-):
+) -> Response:
     return {"result": "success"}
 
 
 @router.post("/limit/short")
 async def limit_short(
     quantity: int,
+    limit: float,
+    expiry_date: str,
+    expiry_time: str,
     symbol: str = Depends(check_symbol),
     user: domain_models.UserDM = Depends(get_current_user_dm),
     db: Session = Depends(get_db),
-):
+) -> Response:
     return {"result": "success"}
 
 
 @router.post("/limit/cover")
 async def limit_cover(
     quantity: int,
+    limit: float,
+    expiry_date: str,
+    expiry_time: str,
     symbol: str = Depends(check_symbol),
     user: domain_models.UserDM = Depends(get_current_user_dm),
     db: Session = Depends(get_db),
-):
+) -> Response:
     return {"result": "success"}

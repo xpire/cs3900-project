14c3c9f2abe6ebe05af4c151e8170d245dd89a8f
Song <z5165313@student.unsw.edu.au>
2020-10-26 09:36:48 +1100

    pending skeleton

--- backend/src/domain_models/orders_dm.py
+++ backend/src/domain_models/orders_dm.py
@@ -1,23 +1,28 @@
+from fastapi.param_functions import Depends
 import src.api.endpoints.stocks as stocks_api
 from sqlalchemy.orm import Session
 from src.core.utilities import log_msg
 from src.crud.crud_user import user
 from src.db.base_model import BaseModel
-from src.game.achievement.achievement import UserAchievement
-from src.game.setup.setup import achievements_list, level_manager
-from src.models import UnlockedAchievement
 from src.schemas import User, UserInDB
-
-# class Name():
-
-#     def update(self, data):
-#         pass
-
-# limit_trade_ex = Name()
-
-# market_data_provider.subscribe(limit_trade_ex)
+from src import domain_models as dm
+from src.core import trade
+from src.schemas.transaction import TradeType
 
 
-class pendingOrder:
+class PendingOrder:
+    def __init__(self, db: Session):
+        self.db = db
+        self.users = user.get_all_users(db)
+
     def update(self, data):
-        pass
+        for investor in self.users:
+            self.execute_pending_orders(investor, self.db)
+
+    def execute_pending_orders(investor: User, db: Session):
+        for order in investor.limit_orders:
+            curr_price = trade.get_stock_price(order.symbol)
+            # TODO: update this to use TRADE TYPE
+            if order.type == TradeType.BUY and order.price < curr_price:
+                transaction = dm.BuyTrade(order.symbol, order.quantity, order.price, db, investor)
+                transaction.execute()

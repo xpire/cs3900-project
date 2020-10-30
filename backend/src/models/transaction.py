c180c08cc856e282620c64ca424f184ceb1620d5
Even Tang <InvalidHero@github.com>
2020-10-30 13:42:36 +1100

    add unique id column

--- backend/src/models/transaction.py
+++ backend/src/models/transaction.py
@@ -1,15 +1,17 @@
 from sqlalchemy import Column, Float, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import PrimaryKeyConstraint
 from sqlalchemy.sql.sqltypes import DateTime
 from src.db.base_model import BaseModel
 
 
 class Transaction(BaseModel):
-    date_time = Column(DateTime, primary_key=True)
-    user_id = Column(String, ForeignKey("user.uid"), primary_key=True)
+    id = Column(Integer, primary_key=True)
+    date_time = Column(DateTime)
+    user_id = Column(String, ForeignKey("user.uid"))
     price = Column(Float, nullable=False)
     action = Column(String)  # buy/sell/short/cover
-    symbol = Column(String, ForeignKey("stock.symbol"), primary_key=True)
+    symbol = Column(String, ForeignKey("stock.symbol"))
     amount = Column(Integer, nullable=False)
     stock_info = relationship(
         "Stock",

80164f3ca1a27e523a65c67572edaad09323bce2
Ian Park <ian.isu.park@gmail.com>
2020-10-30 13:23:50 +1100

    merged with master

--- backend/src/domain_models/trading_hours.py
+++ backend/src/domain_models/trading_hours.py
@@ -11,16 +11,16 @@ class TradingHoursManager:
         curr_datetime = datetime.now(timezone(exchange.timezone))
         date, time = curr_datetime.date(), as_delta(curr_datetime.time())
 
-        start = exchange.start
-        end = exchange.end
+        open = exchange.open
+        close = exchange.close
 
-        is_in_range = start <= time and time <= end
+        is_in_range = open <= time and time <= close
         return is_in_range and self.is_trading_day(stock, date)
 
     def get_trading_hours_info(self, stock):
         exchange = self.get_exchange(stock.exchange)
         is_trading = self.is_trading(self, stock)
-        return dict(is_trading=is_trading, start=exchange.start, end=exchange.end)
+        return dict(is_trading=is_trading, open=exchange.open, close=exchange.close)
 
     def is_trading_day(self, stock, date):
         exchange = self.get_exchange(stock.exchange)  # TODO this should later return a proper db object
@@ -43,66 +43,20 @@ def next_weekday(date):
     return date + timedelta(days=days)
 
 
-def next_open(datetime, open_time):
+def next_open(datetime, exchange):
     d = datetime.date()
     t = datetime.time()
 
-    # BUG for simulated
     # if it is weekday and current time is before open, then next open is today at [open_time]
     # otherwise, next open is on next trading day at [open_time]
-    if not (is_weekday(d) and t < open_time):
-        d = next_weekday(d)
+    if exchange.is_simulated:
+        if t >= exchange.open:
+            d += timedelta(days=1)
+    else:
+        if not is_weekday(d) or t >= exchange.open:
+            d = next_weekday(d)
 
-    return datetime.combine(d, open_time)
+    return datetime.combine(d, exchange.open)
 
 
 trading_hours_manager = TradingHoursManager()
-
-
-# TEST CODE
-# time1 = datetime.now() + timedelta(hours=9)
-# open_time = time(10, 0)
-
-# for i in range(10):
-#     print("======")
-#     print(time1, time1.weekday())
-#     print(next_open(time1, open_time), next_open(time1, open_time).weekday())
-#     time1 = time1 + timedelta(days=1)
-
-
-"""
-class TradingHoursManager:
-    def __init__(self, trading_hours):
-        self.trading_hours = trading_hours
-
-    def get_trading_hours_info(self, stock):
-        if stock.exchange not in self.trading_hours:
-            raise HTTP400("Exchange for the given symbol not found.")
-
-        curr_time = datetime.utcnow().time()
-
-        hours = self.trading_hours[stock.exchange]
-        start, end = hours["start"], hours["end"]
-        is_weekday = datetime.now(hours["timezone"]).weekday() <= 4
-
-        if start <= end:
-            is_in_range = start <= curr_time and curr_time <= end
-        else:
-            is_in_range = start <= curr_time or curr_time <= end
-
-        is_trading = is_weekday and is_in_range
-        return dict(is_trading=is_trading, start=start, end=end)
-
-
-# Trading hours retrieved from
-# https://www.thebalance.com/stock-market-hours-4773216#:~:text=Toronto%20Stock%20Exchange-,9%3A30%20a.m.%20to%204%20p.m.,30%20p.m.%20to%209%20p.m.&text=8%3A30%20a.m.%20to%203%20p.m.
-# TODO fix times, and make start and end times be according to their own timezones...?
-trading_hours_manager = TradingHoursManager(
-    dict(
-        ASX=dict(start=time(23, 0), end=time(5, 0), timezone=timezone("Australia/Sydney")),
-        NYSE=dict(start=time(13, 30), end=time(20, 0), timezone=timezone("America/New_York")),
-        NASDAQ=dict(start=time(13, 30), end=time(20, 0), timezone=timezone("America/New_York")),
-        LSE=dict(start=time(8, 0), end=time(16, 30), timezone=timezone("Europe/London")),
-    )
-)
-"""

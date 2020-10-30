ba1dcf54c651e1c5ec634b00cb4e7a8707c124ee
Song <z5165313@student.unsw.edu.au>
2020-10-30 14:57:53 +1100

    orders api added

--- backend/src/api/api.py
+++ backend/src/api/api.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
-from src.api.endpoints import (auth, leaderboard, portfolio, stocks, trade,
-                               transactions, watchlist)
+from src.api.endpoints import auth, leaderboard, portfolio, stocks, trade, transactions, watchlist, orders
 from src.db import base_model_import_all as base_model
 from src.db.session import engine
 
@@ -12,9 +11,6 @@ api_router.include_router(auth.router, prefix="/user", tags=["user"])
 api_router.include_router(watchlist.router, prefix="/watchlist", tags=["watchlist"])
 api_router.include_router(portfolio.router, prefix="/portfolio", tags=["portfolio"])
 api_router.include_router(trade.router, prefix="/trade", tags=["trade"])
-api_router.include_router(
-    leaderboard.router, prefix="/leaderboard", tags=["leaderboard"]
-)
-api_router.include_router(
-    transactions.router, prefix="/transactions", tags=["transactions"]
-)
+api_router.include_router(leaderboard.router, prefix="/leaderboard", tags=["leaderboard"])
+api_router.include_router(transactions.router, prefix="/transactions", tags=["transactions"])
+api_router.include_router(orders.router, prefix="/orders", tags=["orders"])

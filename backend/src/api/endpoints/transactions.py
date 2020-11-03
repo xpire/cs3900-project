7a12839263e4aa97b42ce52129a86fde751d77fe
Ian Park <ian.isu.park@gmail.com>
2020-11-03 16:39:24 +1100

    remove comments

--- backend/src/api/endpoints/transactions.py
+++ backend/src/api/endpoints/transactions.py
@@ -8,37 +8,6 @@ from src.schemas.transaction import TransactionAPIout
 
 router = APIRouter()
 
-"""
-class Transaction(BaseModel):
-    id = Column(Integer, primary_key=True)
-    user_id = Column(String, ForeignKey("user.uid"))
-    symbol = Column(String, ForeignKey("stock.symbol"))
-    qty = Column(Integer, nullable=False)
-    price = Column(Float, nullable=False)
-    trade_type = Column(String)  # buy/sell/short/cover
-    timestamp = Column(DateTime)
-    stock = relationship(
-        "Stock",
-        backref="transactions",
-        cascade="save-update, merge",
-    )
-"""
-
-"""
-    for transaction in user.model.transaction_hist:
-        ret.append(
-            {
-                "symbol": transaction.symbol,
-                "name": transaction.stock.name,
-                "amount": transaction.qty,
-                "price": transaction.price,
-                "value": transaction.qty * transaction.price,
-                "t_type": transaction.trade_type,
-                "timestamp": transaction.timestamp,
-            }
-        )
-"""
-
 
 @router.get("/")
 async def get_transactions(user_m: User = Depends(get_current_user_m)) -> List[TransactionAPIout]:

52fbb73d6e15522a1eab1231ae824e0ac79d5968
Even Tang <InvalidHero@github.com>
2020-11-04 11:41:14 +1100

    test_stocks add line

--- backend/src/tests/api/test_stocks.py
+++ backend/src/tests/api/test_stocks.py
@@ -1,10 +1,11 @@
 from typing import Dict
 
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
 from src.core.config import settings
 
 
-def test_random_template(client: TestClient):
+def test_random_template(db: Session, client: TestClient):
     r = client.get("stocks/symbols")
 
     d = r.json()

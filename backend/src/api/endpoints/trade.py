25b58bcd21d5ad31f30f40ac7a711ea0fdbafde8
Song <z5165313@student.unsw.edu.au>
2020-11-08 16:39:09 +1100

    src/api/endpoints docstrings done

--- backend/src/api/endpoints/trade.py
+++ backend/src/api/endpoints/trade.py
@@ -7,8 +7,18 @@ from src.schemas.transaction import TradeType
 
 router = ResultAPIRouter()
 
-# TODO change endpoint parameter names
+
 def market_order_endpoint(endpoint, trade_type):
+    """Wrapper function for API market orders
+
+    Args:
+        endpoint (str): market endpoint to hit
+        trade_type (TradeType): type of market trade to make
+
+    Returns:
+        MarketOrder: market order endpoint return
+    """
+
     @router.post(endpoint)
     async def market_order(
         quantity: int,
@@ -16,12 +26,33 @@ def market_order_endpoint(endpoint, trade_type):
         user: dm.UserDM = Depends(get_current_user_dm),
         db: Session = Depends(get_db),
     ) -> Result:
+        """API endpoint to make a market order
+
+        Args:
+            quantity (int): amount of the stock to trade
+            symbol (str, optional): stock symbol. Defaults to Depends(check_symbol).
+            user (dm.UserDM, optional): user domain model. Defaults to Depends(get_current_user_dm).
+            db (Session, optional): database session. Defaults to Depends(get_db).
+
+        Returns:
+            Result: Success/Fail
+        """
         return dm.MarketOrder(symbol=symbol, qty=quantity, user=user, db=db, trade_type=trade_type).submit()
 
     return market_order
 
 
 def limit_order_endpoint(endpoint, trade_type):
+    """Wrapper function for API limit orders
+
+    Args:
+        endpoint (str): limit API to hit
+        trade_type (TradeType): type of limit order to palce
+
+    Returns:
+        LimitOrder: limit_order endpoint return
+    """
+
     @router.post(endpoint)
     async def limit_order(
         quantity: int,
@@ -30,6 +61,18 @@ def limit_order_endpoint(endpoint, trade_type):
         user: dm.UserDM = Depends(get_current_user_dm),
         db: Session = Depends(get_db),
     ) -> Result:
+        """API endpoint to make limit orders
+
+        Args:
+            quantity (int): amount of the stock to trade
+            limit (float): limit price for the order to be executed at
+            symbol (str, optional): stock symbol. Defaults to Depends(check_symbol).
+            user (dm.UserDM, optional): user domain model. Defaults to Depends(get_current_user_dm).
+            db (Session, optional): database session. Defaults to Depends(get_db).
+
+        Returns:
+            Result: Success/Fail
+        """
         return dm.LimitOrder(
             limit_price=limit, symbol=symbol, qty=quantity, user=user, db=db, trade_type=trade_type
         ).submit()

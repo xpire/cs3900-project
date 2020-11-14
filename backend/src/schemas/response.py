7aec0860ca18b068d4ad07395e16cf8589ad2483
Ian Park <ian.isu.park@gmail.com>
2020-11-14 19:49:26 +1100

    refined docs for result system

--- backend/src/schemas/response.py
+++ backend/src/schemas/response.py
@@ -1,5 +1,8 @@
 """
-Scehma for responses to frontend
+System for error and response handling internally and for API responses.
+
+The system is inspried from the Or_error.t monad from OCaml and the Error monad from Haskell, 
+but implemented in a Pythonic way.
 """
 
 from functools import wraps
@@ -16,13 +19,16 @@ class Response(BaseSchema):
 
 
 class Result(BaseSchema):
-    """Internal return type"""
+    """Generic internal return type"""
 
     msg: str = ""
     success: bool
     data: Optional[Any]
 
     def log(self, log_level=None):
+        """
+        Record the contained message to the logging system
+        """
         if self.success:
             log_level = log_level or "INFO"
         else:
@@ -32,12 +38,18 @@ class Result(BaseSchema):
         return self
 
     def as_response(self):
+        """
+        Convert [self] to an API response (a message or an exception)
+        """
         if self.success:
             return Response(msg=self.msg)
         else:
             raise HTTP400(self.msg)
 
     def ok(self):
+        """
+        Return data if successful otherwise raise
+        """
         if self.success:
             return self.data
         else:
@@ -60,13 +72,23 @@ RaiseFail = lambda msg="", data=None: Fail(msg, data).as_response()
 
 
 class ResultException(Exception):
+    """
+    An exception that can be used to short circuit through blocks of code, as explained
+    in [result_result()] and [ResultAPIRouter]
+
+    This allows for clean syntax in code.
+    """
+
     def __init__(self, result):
         super().__init__()
         self.result = result
 
 
 def return_result():
-    """Allows short circuiting of results - handles ResultException"""
+    """
+    Allows short circuiting of results - catches ResultException and converts it back into a
+    failure [Result] instance. See [ResultAPIRouter] for how it is exactly used and its effects.
+    """
 
     def wrapper(fn):
         @wraps(fn)
@@ -83,7 +105,9 @@ def return_result():
 
 
 def method_wrapper(router_method):
-    """Given a router method (e.g. router.get) this modifies the method such that the routed endpoint functions are automatically decorated by return_result
+    """
+    Given a router method (e.g. router.get) this modifies the method such that the routed endpoint functions
+    are automatically decorated by return_result(). See [ResultAPIRouter] for how it is exactly used and its effects.
 
     Args:
         router_method: HTTP router method (get/post/delete etc.)
@@ -108,6 +132,29 @@ def method_wrapper(router_method):
 
 
 def ResultAPIRouter():
+    """
+    Returns fastAPI's APIRouter, but its methods are patched so that they are
+    automatically decorated with [return_result()].
+
+    This allows coding like following:
+
+    ```
+    @router.delete("")
+    async def delete_watchlist(
+        symbol: str = Depends(check_symbol),
+        user_m: models.user = Depends(get_current_user_m),
+        db: Session = Depends(get_db),
+    ) -> Result:
+        crud.user.delete_from_watchlist(symbol=symbol, db=db, user=user_m).ok()
+        return Success(f"{symbol} removed from watchlist")
+    ```
+
+    The above endpoint calls .ok() on the result of crud operation. If the result was
+    a failure, then it would raise a ResultException, but because @router.delete("")
+    automatically wraps the endpoint function in [@return_result()], this exception
+    would be safely caught and converted into an error message (i.e. HTTP400 exception with
+    its error message corresponding to that contained in the failure result)
+    """
     from fastapi import APIRouter
 
     router = APIRouter()

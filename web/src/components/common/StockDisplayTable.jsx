815684088826720cca810eb16a6cd5f193267948
Ian Park <ian.isu.park@gmail.com>
2020-11-16 01:14:57 +1100

    reduxified stock details table

--- web/src/components/common/StockDisplayTable.jsx
+++ web/src/components/common/StockDisplayTable.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Tooltip } from "@material-ui/core";
+import { Tooltip, Typography } from "@material-ui/core";
 import {
   Divider,
   List,
@@ -35,45 +35,22 @@ const StockDisplayTable = ({
       {Object.keys(displayValues).map((key) => (
         <>
           <List>
-            <ListItem key={key} style={{ padding: "4px 0px" }}>
-              <ListItemText
-                primary={key}
-                style={{ float: "left", color: "grey" }}
-              />
+            <ListItem
+              key={key}
+              style={{ padding: "4px 0px" }}
+              style={{ display: "block" }}
+            >
+              <Typography variant="caption" color="textSecondary">
+                {key}
+              </Typography>
 
-              {key !== "Full Name" ? (
-                <ListItemSecondaryAction
-                  style={{
-                    position: "absolute",
-                    right: "0px",
-                  }}
-                >
-                  <ListItemText
-                    primary={displayValues[key]}
-                    color="secondary"
-                  />
-                </ListItemSecondaryAction>
-              ) : renderStatus ? (
-                <ListItemSecondaryAction
-                  style={{
-                    position: "absolute",
-                    right: "0px",
-                  }}
-                >
-                  <ListItemText primary={displayValues[key]} />
-                </ListItemSecondaryAction>
+              {key !== "Full Name" || renderStatus ? (
+                <Typography>{displayValues[key]}</Typography>
               ) : (
                 <Tooltip
                   title={fullname == undefined ? "placeholder" : fullname}
                 >
-                  <ListItemSecondaryAction
-                    style={{
-                      position: "absolute",
-                      right: "0px",
-                    }}
-                  >
-                    <ListItemText primary={displayValues[key]} />
-                  </ListItemSecondaryAction>
+                  <Typography>{displayValues[key]}</Typography>
                 </Tooltip>
               )}
             </ListItem>
@@ -123,3 +100,63 @@ const StockDisplayTable = ({
 };
 
 export default StockDisplayTable;
+
+/*
+const StockFieldItem = (label) => {
+  return (
+    <ListItemSecondaryAction
+      style={{
+        position: "absolute",
+        right: "0px",
+      }}
+    >
+      <ListItemText primary={label} color="secondary" />
+    </ListItemSecondaryAction>
+  );
+};
+
+const StockDisplayTable = ({
+  name,
+  exchange,
+  industry,
+  currency,
+  type,
+  fullname,
+  renderStatus,
+}) => {
+  const displayValues = {
+    "Full Name": name,
+    Exchange: exchange,
+    Industry: industry,
+    Currency: currency,
+    Type: type,
+  };
+
+  return (
+    <>
+      <Divider />
+      {Object.keys(displayValues).map((key) => (
+        <>
+          <List>
+            <ListItem key={key} style={{ padding: "4px 0px" }}>
+              <Typography variant="caption" color="textSecondary">
+                {key}
+              </Typography>
+
+              {key !== "Full Name" || renderStatus ? (
+                <StockFieldItem label={displayValues[key]} />
+              ) : (
+                <Tooltip
+                  title={fullname == undefined ? "placeholder" : fullname}
+                >
+                  <StockFieldItem label={displayValues[key]} />
+                </Tooltip>
+              )}
+            </ListItem>
+          </List>
+          <Divider />
+        </>
+      ))}
+    </>
+  );
+  */
